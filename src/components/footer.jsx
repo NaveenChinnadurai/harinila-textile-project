ef1582704623399fce581d356d41e5b93173293f
Naveen Chinnadurai <dev.iamnaveen@gmail.com>
2024-03-26 00:14:26 +0530

    modified:footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,16 +4,18 @@ import { FaPhoneAlt as Phone} from "react-icons/fa";
 import { MdEmail as Mail} from "react-icons/md";
 import { FaLocationDot as Location } from "react-icons/fa6";
 import { LiaCopyrightSolid as Copyrights} from "react-icons/lia";
+import { IoIosArrowDown as Arrow } from "react-icons/io";
+
 function Footer() {
     return (
-        <div className='row col-10 mx-auto mt-4 d-flex justify-content-between px-2  px-md-5 pt-4 pb-2 rounded-bottom rounded-5 footer-bg'>
-            <div className='d-flex flex-column justify-content-between'>
+        <div className='row col-10 mx-auto d-flex justify-content-between px-5 pt-5 pb-1 rounded-bottom rounded-5 footer-bg'>
+            <div className='d-flex justify-content-between'>
                 <div className="col-3 d-flex justify-content-center align-items-center">
                     <img src={iconImg} alt="Icon Images" height={120} width={120} />
                 </div>
                 <ul className="col-2 d-flex flex-column justify-content-center">
                     <li className='fs-6 lead fw-normal'>Home</li>
-                    <li className='fs-6 lead fw-normal'>Product</li>
+                    <li className='fs-6 lead fw-normal'>Product <Arrow/></li>
                     <li className='fs-6 lead fw-normal'>About</li>
                     <li className='fs-6 lead fw-normal'>Contact</li>
                 </ul>
@@ -29,7 +31,7 @@ function Footer() {
                     <li className='fs-6 lead fw-normal gap-1'><Location/>12, 1st St Jayabharathi Nagar, <br /> Porur,Chennai - 600116, India</li>
                 </ul>
             </div>
-            <p className="lead fs-5 fw-normal mt-3 d-flex align-items-center justify-content-center"><Copyrights className='fw-bold fs-4'/>2024 All rights are Reserved by Harinila Textiles</p>
+            <p className="lead fs-6 fw-normal mt-3 d-flex align-items-center justify-content-center"><Copyrights className='fw-bold fs-5'/>2024 All rights are Reserved by Harinila Textiles</p>
         </div>
     )
 }
