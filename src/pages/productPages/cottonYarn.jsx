fc68dacb3c8b0050a356f3b01473aeacc8c947c3
Naveen Chinnadurai <dev.iamnaveen@gmail.com>
2024-05-18 19:29:29 +0530

    updated:product data file

--- src/pages/productPages/cottonYarn.jsx
+++ src/pages/productPages/cottonYarn.jsx
@@ -1,73 +1,14 @@
 import ProductPage from '../productPage'
-
-
-import mainBanner from "../../assets/banners/cottonBanner.png"
-
-import banner1 from "../../assets/banners/cottonBanner/img1.png"
-import banner2 from "../../assets/banners/cottonBanner/img2.png"
-import banner3 from "../../assets/banners/cottonBanner/img3.png"
-
-import img1 from "../../assets/stocks/cottonStock/img1.png"
-import img2 from "../../assets/stocks/cottonStock/img2.png"
-import img3 from "../../assets/stocks/cottonStock/img3.png"
-import img4 from "../../assets/stocks/cottonStock/img4.png"
-import img5 from "../../assets/stocks/cottonStock/img5.png"
-import img6 from "../../assets/stocks/cottonStock/img6.png"
+import cottonData from '../../scripts/cottonyarn'
 
 function CottonYarn() {
     return (
         <>
             <ProductPage
-                data={cottonData[0]}
+                data={cottonData}
             />
         </>
     )
 }
 
-const cottonData = [
-    {
-      headerTitle1: "Cotton Yarn",
-      headerTitle2: "Harinila Textiles - Cotton Yarn Spinning",
-      bannerImg: mainBanner,
-      bannerText: "Welcome to Harinila Textiles! Sustainable Cotton Yarns & Fabrics",
-      bannerSubText: "Innovation in your business starts here",
-      bannerImages: [banner1, banner2, banner3],
-      para1: "We are a leading supplier of premium quality cotton yarn for the global market. With our commitment to excellence and customer satisfaction, we have established ourselves as a trusted partner in the textile industry. Whether you are a garment manufacturer, a wholesaler, or a textile company, we have the right cotton yarn to meet your needs.",
-      para2: "Our extensive range of cotton yarns is meticulously crafted to meet the highest industry standards, ensuring superior quality and performance in every strand. From fine counts for delicate fabrics to robust options for durable textiles, we offer versatile solutions tailored to your specific requirements. Partner with us and experience the difference that premium quality cotton yarn can make in your products.",
-      feature: [
-        {
-          title: "High-Quality Cotton Yarn",
-          path: img1,
-          text: "We offer a wide range of cotton yarn options, carefully sourced from reputable manufacturers. Our yarns are known for their superior quality, durability, and consistency, making them ideal for various applications."
-        },
-        {
-          title: "Customization Options",
-          path: img2,
-          text: "We understand that each customer has unique requirements. That's why we provide customization options such as yarn count, twist, and color. We work closely with our clients to deliver tailored solutions that match their specifications."
-        },
-        {
-          title: "International Standards",
-          path: img3,
-          text: "We adhere to stringent quality standards to ensure that our cotton yarns meet international industry norms. Our commitment to quality control guarantees that you receive yarns that are consistent in texture, strength, and color."
-        },
-        {
-          title: "Reliable Supply",
-          path: img4,
-          text: "We have established strong relationships with reputed cotton yarn manufacturers, enabling us to provide a consistent supply of high-quality yarns. Our efficient logistics ensure timely delivery to any destination worldwide."
-        },
-        {
-          title: "Competitive Pricing",
-          path: img5,
-          text: "We offer competitive pricing without compromising on quality. Our aim is to provide cost-effective solutions that meet your budget requirements and help you achieve profitability."
-        },
-        {
-          title: "Global Reach",
-          path: img6,
-          text: "We have a strong presence in the international market, with a network of satisfied customers across different continents. Our export expertise allows us to efficiently handle documentation, shipping, and customs procedures."
-        }
-      ],
-      Color: "#7dc391",
-      contactText: "Contact us today to discuss your cotton yarn requirements or to request samples. We look forward to partnering with you and delivering the finest quality cotton yarn for your textile needs."
-    },
-  ];
 export default CottonYarn
---
+++ src/scripts/wovenfabric.js
@@ -0,0 +1,62 @@
+
+
+import mainBanner from "../assets/banners/wovenFabric.png"
+import banner1 from "../assets/banners/wovenBanner/img1.png"
+import banner2 from "../assets/banners/wovenBanner/img2.png"
+import banner3 from "../assets/banners/wovenBanner/img3.png"
+
+import img1 from '../assets/stocks/knittedStocks/img1.png'
+import img2 from "../assets/stocks/knittedStocks/img2.png"
+import img3 from "../assets/stocks/knittedStocks/img3.png"
+import img4 from "../assets/stocks/knittedStocks/img4.png"
+import img5 from "../assets/stocks/knittedStocks/img5.png"
+import img6 from "../assets/stocks/knittedStocks/img6.png"
+
+
+const wovenFabricData = {
+    headerTitle1: "Woven Fabrics",
+    headerTitle2: "Harinila Textiles - Woven Fabric Crafting",
+    bannerImg: mainBanner,
+    bannerText: "Elevate Your Creations Discover Exquisite Woven Fabrics, Unmatched Quality & Sustainability",
+    bannerImages: [banner1, banner2, banner3],
+    bannerSubText: "Future of Textiles industry begin Here",
+    para1: "Discover superior woven fabrics at Harinila Textiles, your ultimate source for premium textile solutions. With our unwavering dedication to quality and customer-centric approach, we strive to be your trusted ally in the textile realm. Whether you're a garment manufacturer, wholesaler, or textile company, effortlessly find the ideal woven fabrics to elevate your projects.",
+    para2: "Experience the pinnacle of innovation and excellence in woven fabric production at Harinila Textiles. Our extensive array of woven fabrics is meticulously engineered to surpass industry benchmarks, guaranteeing unmatched quality and functionality with each weave. Whether you seek intricate patterns or enduring textures, our versatile solutions are customized to your exacting specifications. Collaborate with us and discover the transformative impact of premium woven fabrics on your creations.",
+    feature: [
+        {
+            title: "Premium Woven Fabric Selection",
+            path: img1,
+            text: "Explore our comprehensive array of woven fabrics, meticulously sourced from trusted manufacturers celebrated for their craftsmanship. Our curated selection of woven fabrics is designed to cater to your varied needs and preferences, promising exceptional quality, durability, and consistency in every thread."
+        },
+        {
+            title: "Customization Options",
+            path: img2,
+            text: "At Harinila Textiles, we understand that each customer is unique. That's why we provide a variety of customization options to ensure that your woven fabric perfectly fits your specifications. From yarn count and twist to color selection, we collaborate closely with our clients to deliver personalized solutions that align with their individual preferences and requirements."
+        },
+        {
+            title: "International Standards",
+            path: img3,
+            text: "At Harinila Textiles, we prioritize stringent quality control measures to ensure that our woven fabric yarns consistently meet international industry standards. Our unwavering commitment to quality guarantees that you receive yarns that adhere to the highest benchmarks of texture, strength, and color consistency."
+        },
+        {
+            title: "Reliable Supply",
+            path: img4,
+            text: "we've cultivated strong partnerships with esteemed knitted fabric manufacturers, guaranteeing a dependable and consistent supply of premium-quality fabrics. Our enduring relationships within the industry empower us to sustain a steady flow of high-quality knitted fabrics, meeting your demands with reliability and excellence."
+        },
+        {
+            title: "Competitive Pricing",
+            path: img5,
+            text: "At Harinila Textiles, quality meets affordability with our fabric yarns. We prioritize competitive pricing without compromising on quality, ensuring cost-effective solutions that align with your budget while upholding the highest standards. Our aim is to provide fabric yarns that not only meet your production needs but also enhance your profitability."
+        },
+        {
+            title: "Global Reach",
+            path: img6,
+            text: "We're everywhere you need us to be. With our extensive reach across continents, we serve a satisfied clientele worldwide. Our export expertise ensures smooth handling of documentation, shipping, and customs procedures, guaranteeing efficient delivery of our premium fabric yarns to your doorstep, wherever you are."
+        }
+    ],
+    color: "#f3c553",
+    contactText: "Contact us today to explore your woven fabric needs or to request samples. We're enthusiastic about the opportunity to collaborate with you and provide top-tier quality fabrics perfectly suited for your projects."
+}
+
+
+export default wovenFabricData
